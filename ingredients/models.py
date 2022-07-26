e7c7b44eb41094ee7a301a698ce717597875e22b
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-07-26 11:10:29 -0300

    feat: created relationships

---
+++ ingredients/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=40)
+
+    recipes = models.ManyToManyField("recipes.Recipe", related_name="ingredients")
+
+    def __repr__(self) -> str:
+        return f"Ingrediente {self.id} | name: {self.name}"
