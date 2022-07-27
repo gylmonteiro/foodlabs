ecf0ae976ad4c16ebf04a7ab2d1d4656172e6c78
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-07-27 11:45:00 -0300

    feat: created serializers

---
+++ ingredients/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class IngredientSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=40)
+
+    # recipes = serializers.ManyToManyField("recipes.Recipe", related_name="ingredients")    
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-
+from addresses.serializers import AddressSerializer
 class UserSerializer(serializers.Serializer):
     first_name= serializers.CharField(max_length=10)
     last_name = serializers.CharField(max_length=10)
     email = serializers.EmailField(max_length=100)
 
+    address = AddressSerializer(read_only=True)
