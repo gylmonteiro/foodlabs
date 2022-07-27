a5dc46545a053b6d1798e7f56dc01ae9713288cc
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-07-27 13:00:21 -0300

    feat: Serializers completeds and urls

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from addresses.serializers import AddressSerializer
+from users.models import User
+from addresses.models import Address
 class UserSerializer(serializers.Serializer):
     first_name= serializers.CharField(max_length=10)
     last_name = serializers.CharField(max_length=10)
     email = serializers.EmailField(max_length=100)
 
-    address = AddressSerializer(read_only=True)
+    address = AddressSerializer()
+
+
+    def create(self, validated_data):
+        address_data = validated_data.pop("address")
+        
+        user = User.objects.create(**validated_data)
+        address = Address.objects.create(**address_data, user= user)
+        return (user)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView, Response, status
+from users.models import User
+from users.serializers import UserSerializer
 # Create your views here.
+class UserView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        list_users_serializer = UserSerializer(users, many=True)
+        return Response(list_users_serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        # if serializer.is_valid():
+        #     user = User.objects.create(**serializer.validate_data)
+        #     user.save()
+        #     return Response(serializer.data)
+        # else:
+        #     errors = serializer.errors
+        #     return Response(errors)
+        serializer.is_valid(raise_exception=True)
+
+        # user = User(**serializer.validated_data)
+        # user.save()
+        # serializer_out = UserSerializer(user)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
